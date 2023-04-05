9ffa850026613d51e504598b7c0cec5f7cbc2098
Anand Makasana <anandmakasana2002@gmail.com>
2023-04-05 11:09:48 +0530

    fix navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import { logo } from "../utils/constants";
 import { SearchBar } from "./";
 
 const Navbar = () => (
-    <Stack direction="row" alignItems="center" pl={3} sx={{ position: "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+    <Stack direction="row" alignItems="center" pl={3} sx={{ position: "sticky", background: '#000', top: 0, justifyContent: "space-between", flexWrap: "wrap" }}>
         <Link to="/" style={{ display: "flex", alignItems: "center" }}>
             <img src={logo} alt="logo" height={70} />
         </Link>
